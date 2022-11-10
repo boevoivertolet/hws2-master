0c72ec33fc802acc5a8caa704b3f753cadd09c93
boevoivertolet <bjha78rus@yandex.ru>
2022-11-10 13:24:37 +0300

    filter Done

--- hws2-master/src/s2-homeworks/hw02/HW2.tsx
+++ hws2-master/src/s2-homeworks/hw02/HW2.tsx
@@ -37,22 +37,20 @@ const defaultAffairs: Array<AffairType> = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-
-
-    return affairs // need to fix
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => {// need to fix any
+    return affairs.filter(el => filter === 'all' ? el : el.priority === filter)// need to fix
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
 
     return affairs // need to fix
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
+    const deleteAffairCallback = (_id: number) => { // need to fix any
         // need to fix
     }
 
