80d54fa65a6a6e52be177238fbb12171035a2a30
boevoivertolet <bjha78rus@yandex.ru>
2023-07-20 11:34:21 +0300

    fix App css

--- hws2-master/src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ hws2-master/src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation} from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {PATH} from '../Pages'
 import closeIcon from './closeOutline.svg'
@@ -10,6 +10,11 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
+    const location = useLocation();
+
+    const isActive = (path: string) => {
+        return location.pathname === path ? s.active : '';
+    }
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
     return (
@@ -31,7 +36,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={isActive(PATH.PRE_JUNIOR)}
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +44,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={isActive(PATH.JUNIOR)}
                     >
                         Junior
                     </NavLink>
@@ -47,7 +52,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={isActive(PATH.JUNIOR_PLUS)}
                     >
                         Junior Plus
                     </NavLink>
